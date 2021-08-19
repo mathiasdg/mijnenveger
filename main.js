8122a9c2afe2b23dc469cd6fb5ef994ea97b242b
Mathias De Gucht <mathias.de.gucht@gmail.com>
2021-08-19 19:15:23 +0200

    gameplay works

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-let rows = 14;
-let cols = 16;
+const size = 7;
+const canvas_size = 500;
+const rows = size;
+const cols = size;
+let GAME_OVER = false;
+let FINISHED = false;
+let draw_size = canvas_size / size;
 let total_mines = 0;
+let total_cells = size * size;
 let grid;
+let grid_clicked;
 
 function make_grid(rows, cols) {
   let grid = new Array(rows);
@@ -66,15 +73,81 @@ function calculate_neighbors(grid) {
   return grid;
 }
 
+function showCell() {
+  let pos_i = Math.floor(mouseX / draw_size);
+  let pos_j = Math.floor(mouseY / draw_size);
+
+  if (GAME_OVER) {
+    console.log("GAME OVER!");
+  } else if (FINISHED) {
+    console.log("FINISHED!");
+  } else {
+    if (!grid_clicked[pos_j][pos_i]) {
+      let cell = grid[pos_j][pos_i];
+      let x = draw_size * pos_i;
+      let y = draw_size * pos_j;
+
+      if (cell === "M") {
+        if (mouseIsPressed) {
+          if (mouseButton === LEFT) {
+            GAME_OVER = true;
+            console.log("BOOOM!");
+            fill(222, 22, 22);
+          }
+          if (mouseButton === RIGHT) {
+            fill(22, 222, 22);
+            console.log("mooie vondst!!");
+          }
+          noStroke();
+          circle(x + draw_size / 2, y + draw_size / 2, draw_size / 2);
+        }
+      } else {
+        if (total_cells - total_mines === 1) {
+          FINISHED = true;
+          console.log("u bent uit!");
+        }
+        noStroke();
+        fill(22);
+        textSize(draw_size * 0.5);
+        textAlign(CENTER, CENTER);
+        if (1) {
+          text(cell, x + draw_size / 2, y + draw_size / 2 + 3);
+        }
+        --total_cells;
+      }
+      grid_clicked[pos_j][pos_i] = true;
+    } else {
+      console.log("reeds geklikt");
+    }
+  }
+}
+
 grid = plant_mines(make_grid(rows, cols));
+grid_clicked = make_grid(rows, cols);
 calculate_neighbors(grid);
-console.table(grid);
+// console.table(grid);
+// console.table(grid_clicked);
 console.log("totale mijnen: " + total_mines);
 
 function setup() {
-  createCanvas(400, 400);
+  let canvas = createCanvas(canvas_size, canvas_size);
+  canvas.mousePressed(showCell);
+
+  for (let element of document.getElementsByClassName("p5Canvas")) {
+    element.addEventListener("contextmenu", e => e.preventDefault());
+  }
 }
 
 function draw() {
-  background(220);
+  for (let i = 0; i < rows; ++i) {
+    for (let j = 0; j < cols; ++j) {
+      let x = draw_size * j;
+      let y = draw_size * i;
+
+      // draw empty start grid
+      stroke(111, 122, 133);
+      noFill();
+      square(x, y, draw_size);
+    }
+  }
 }

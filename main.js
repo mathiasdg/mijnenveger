beba2cc9bda6c6a2f0f0745a7b5245393c0dda85
Mathias De Gucht <mathias.de.gucht@gmail.com>
2021-08-20 16:14:14 +0200

    added showAllCells function for end of game

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const size = 7;
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+const size = 5;
 const canvas_size = 500;
 const rows = size;
 const cols = size;
@@ -72,52 +74,105 @@ function calculate_neighbors(grid) {
 
   return grid;
 }
+function showAllCells() {
+  for (let i = 0; i < rows; ++i) {
+    for (let j = 0; j < cols; ++j) {
+      let x = draw_size * j;
+      let y = draw_size * i;
+      let cell = grid[i][j];
 
-function showCell() {
-  let pos_i = Math.floor(mouseX / draw_size);
-  let pos_j = Math.floor(mouseY / draw_size);
-
-  if (GAME_OVER) {
-    console.log("GAME OVER!");
-  } else if (FINISHED) {
-    console.log("FINISHED!");
-  } else {
-    if (!grid_clicked[pos_j][pos_i]) {
-      let cell = grid[pos_j][pos_i];
-      let x = draw_size * pos_i;
-      let y = draw_size * pos_j;
+      // draw background when cell clicked
+      fill(222);
+      noStroke();
+      square(x, y, draw_size);
 
       if (cell === "M") {
-        if (mouseIsPressed) {
-          if (mouseButton === LEFT) {
-            GAME_OVER = true;
-            console.log("BOOOM!");
-            fill(222, 22, 22);
-          }
-          if (mouseButton === RIGHT) {
-            fill(22, 222, 22);
-            console.log("mooie vondst!!");
-          }
-          noStroke();
-          circle(x + draw_size / 2, y + draw_size / 2, draw_size / 2);
-        }
+        fill(222, 22, 22);
+        circle(x + draw_size / 2, y + draw_size / 2, draw_size / 2);
       } else {
-        if (total_cells - total_mines === 1) {
-          FINISHED = true;
-          console.log("u bent uit!");
-        }
+        // draw text with amount of mines around the cell
         noStroke();
         fill(22);
         textSize(draw_size * 0.5);
         textAlign(CENTER, CENTER);
-        if (1) {
+        if (cell !== 0) {
           text(cell, x + draw_size / 2, y + draw_size / 2 + 3);
         }
-        --total_cells;
       }
-      grid_clicked[pos_j][pos_i] = true;
+    }
+  }
+}
+
+function showCell() {
+  let pos_i = Math.floor(mouseX / draw_size);
+  let pos_j = Math.floor(mouseY / draw_size);
+  let x = draw_size * pos_i;
+  let y = draw_size * pos_j;
+
+  if (GAME_OVER) {
+    console.log("GAME OVER!");
+    alert("GAME OVER!");
+  } else if (FINISHED) {
+    console.log("FINISHED!");
+    alert("U BENT UIT! WOEW!");
+  } else {
+    if (!grid_clicked[pos_j][pos_i]) {
+      let cell = grid[pos_j][pos_i];
+
+      if (mouseIsPressed) {
+        // draw background when cell clicked
+        fill(222);
+        noStroke();
+        square(x, y, draw_size);
+        if (mouseButton === RIGHT) {
+          fill(22, 222, 22);
+          stroke(111, 122, 133);
+          circle(x + draw_size / 2, y + draw_size / 2, draw_size / 2);
+
+          console.log("gemarkeerd");
+        } else {
+          if (cell === "M") {
+            GAME_OVER = true;
+            fill(222, 22, 22);
+            circle(x + draw_size / 2, y + draw_size / 2, draw_size / 2);
+
+            console.log("BOOOM!");
+            alert("GAME OVER!");
+            showAllCells();
+
+            noStroke();
+          } else {
+            if (total_cells - total_mines === 1) {
+              FINISHED = true;
+              console.log("u bent uit!");
+              alert("U BENT UIT!");
+            }
+
+            // draw text with amount of mines around the cell
+            noStroke();
+            fill(22);
+            textSize(draw_size * 0.5);
+            textAlign(CENTER, CENTER);
+            if (cell !== 0) {
+              text(cell, x + draw_size / 2, y + draw_size / 2 + 3);
+            }
+            --total_cells;
+          }
+        }
+        grid_clicked[pos_j][pos_i] = true;
+      }
     } else {
       console.log("reeds geklikt");
+
+      if (mouseButton === RIGHT) {
+        erase();
+        fill(22, 222, 22);
+        circle(x + draw_size / 2, y + draw_size / 2, draw_size / 2);
+        noErase();
+        grid_clicked[pos_j][pos_i] = false;
+
+        console.log("markering weg?");
+      }
     }
   }
 }
@@ -125,8 +180,8 @@ function showCell() {
 grid = plant_mines(make_grid(rows, cols));
 grid_clicked = make_grid(rows, cols);
 calculate_neighbors(grid);
-// console.table(grid);
-// console.table(grid_clicked);
+console.table(grid);
+console.table(grid_clicked);
 console.log("totale mijnen: " + total_mines);
 
 function setup() {
